f99dea0aca504603c257cb94d6c79848c19d767d
Yixing Wang <wyx@yixings-macbook-pro.local>
2019-03-04 00:43:45 -0800

    login and logout

--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -16,7 +16,17 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func onLoginButton(_ sender: Any) {
+        
+        let myUrl = "https://api.twitter.com/oauth/request_token"
+        
+        TwitterAPICaller.client?.login(url: myUrl, success: {
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }, failure: { (Error) in
+            print("Could not log in!")
+        })
+    }
+    
     /*
     // MARK: - Navigation
 
