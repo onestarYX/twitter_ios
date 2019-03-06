db4f610dce903f592d28407ed5041de265febcbe
Yixing Wang <wyx@wireless-169-228-116-83.ucsd.edu>
2019-03-06 14:11:36 -0800

    part 2 beginning

---
+++ Twitter/TweetViewController.swift
@@ -0,0 +1,38 @@
+//
+//  TweetViewController.swift
+//  Twitter
+//
+//  Created by Yixing Wang on 3/5/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func tweet(_ sender: Any) {
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
